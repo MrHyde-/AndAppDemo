dcbcde7afecea00de2ff2c0bb6043c560abb8de1
MrHyde- <antti.karjakin@gmail.com>
2014-04-11 21:45:59 +0300

    added db basics

---
+++ src/amk/database/AkBaDb.java
@@ -0,0 +1,21 @@
+package amk.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class AkBaDb {
+    public SQLiteDatabase database;
+    private DbHelper dbHelper;
+    
+    public AkBaDb(Context context) {
+    	dbHelper = new DbHelper(context);
+    }
+
+    public void open() {
+    	database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+    	dbHelper.close();
+    }
+}
---
+++ src/amk/database/DbHelper.java
@@ -0,0 +1,56 @@
+package amk.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+	private static final String DATABASE_NAME = "db.akBaDb";
+
+	public static final String TABLE_BACKLOG = "BACKLOG";
+	public static final String TABLE_TODO = "TODO";
+
+	public static final String COLUMN_ID = "id";
+	public static final String COLUMN_NAME = "name";
+	public static final String COLUMN_BACKLOG = "backlogid";
+	public static final String COLUMN_STATUS = "status";
+	public static final String COLUMN_ADDED = "createdDate";
+
+	private static final int DATABASE_VERSION = 1;
+
+	public static final String CREATE_TABLEBACKLOG = "create table "
+			+ TABLE_BACKLOG + "(" + COLUMN_ID
+			+ " integer primary key autoincrement," + COLUMN_NAME + " text not null);";
+
+	public static final String CREATE_TABLETODOS = "create table "
+			+ TABLE_TODO + "(" + COLUMN_ID
+			+ " integer primary key autoincrement, " + COLUMN_NAME
+			+ " text not null, " + COLUMN_BACKLOG
+			+ " integer not null, " + COLUMN_STATUS
+			+ " integer not null, " + COLUMN_ADDED
+			+ " default current_timestamp);";
+
+	public DbHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(CREATE_TABLEBACKLOG);
+		db.execSQL(CREATE_TABLETODOS);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		Log.w(DbHelper.class.getName(),
+				"Upgrading database from version " + oldVersion + " to "
+						+ newVersion + ", which will destroy all old data");
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_BACKLOG);
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_TODO);
+		onCreate(db);
+
+	}
+
+}
